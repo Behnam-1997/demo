4df1baeb0a9016b6cd1548671d711da525128fc6
Behnam Rabizadeh <behnam.limoo@gmail.com>
2022-11-20 22:33:05 +0330

    fifth

--- cypress/pages/widgetsPage.pom.js
+++ cypress/pages/widgetsPage.pom.js
@@ -1,5 +1,5 @@
-const commonData = {};
-
-const elements = {};
-
-export const visit = () => {};
+export const progressBarBtn = () => cy.contains("Progress Bar");
+export const toolTipsBtn = () => cy.contains("Tool Tips");
+export const progressBar = () => cy.get("#progressBar");
+export const startBtn = () => cy.get("#startStopButton");
+export const resetBtn = () => cy.get("#resetButton");
